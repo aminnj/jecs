9549c74d8555696530383537820ef75577bbeda9
Nick Amin <amin.nj@gmail.com>
2019-01-29 20:45:53 -0800

    back to fine binning

--- l2l3/commands.sh
+++ l2l3/commands.sh
@@ -19,19 +19,19 @@ mkdir -p text/
 
 ##### 0 #####
 
-# # copy L1 corrections here and make the dummy L3 corrections which will be needed by a script later
-# cp ../l1/text/*.txt .
-# for algo in $algos; do
-#     alias=$(python -c "print '$algo'.replace('ak','AK').replace('pf','PF')")
-#     echo -e "{1         JetEta              1          JetPt               1     Correction     L3Absolute}\n -5.191          5.191              2              4           5000" >  ${era}_L3Absolute_${alias}.txt
-# done
+# copy L1 corrections here and make the dummy L3 corrections which will be needed by a script later
+cp ../l1/text/*.txt .
+for algo in $algos; do
+    alias=$(python -c "print '$algo'.replace('ak','AK').replace('pf','PF')")
+    echo -e "{1         JetEta              1          JetPt               1     Correction     L3Absolute}\n -5.191          5.191              2              4           5000" >  ${era}_L3Absolute_${alias}.txt
+done
 
 ##### 1 #####
 
 # make jra_f.root for L2L3
 for algo in $algos; do
     # jet_response_analyzer_x ../../config/jra_dr_finebinning.config -input $basepath/JRA_PUFlat_jec.root -presel "jte<6500" -algs ak4pfchsl1
-    jet_response_analyzer_x ../../config/jra_dr.config -input $basepath/$flatPUL1cor   -algs ${algo}l1
+    jet_response_analyzer_x ../../config/jra_dr_finebinning.config -input $basepath/$flatPUL1cor   -algs ${algo}l1
     jet_response_fitter_x -input jra.root -output jra_f_${algo}.root -niter 10 -nsigma 1.5
 done
 
